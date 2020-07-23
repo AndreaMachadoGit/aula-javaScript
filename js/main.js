26e914dbd5e420a07a56c53d16a98372ed8c023f
Andrea Machado <andreamgaropaba@gmail.com>
2020-07-23 13:32:52 -0300

    1st commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+var nome = "Andrea Machado";
+var idade = 49;
+var frase = "Japão é o melhor time do mundo!";
+var lista = ["maçã","pera","laranja"];
+lista.push("uva");
+console.log(lista);
+lista.pop();
+console.log(lista);
+console.log(lista.lenght);
+console.log(lista[1]);
+alert(lista[2]);
+//alert("meu primeiro js, feito com include na header do index.html");
+//alert("Bem vinda " + nome);
+//alert(nome + " está com " + idade + " anos.")
+//console.log(nome);
+//console.log(idade);
+//console.log(frase.toUpperCase());
+//console.log(frase.replace("Japão", "Brasil"));
